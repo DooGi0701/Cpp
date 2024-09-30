b13960be7f5216fec0f4038c493a8d95caa91b8d
DooGi0701 <96532783+DooGi0701@users.noreply.github.com>
2024-09-30 21:29:29 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Ram.h"
+
+int main() {
+    Ram ram;
+    ram.write(100, 20);     
+    ram.write(101, 30);     
+
+    char res = ram.read(100) + ram.read(101);  
+    ram.write(102, res);     
+
+    std::cout << "102 번지의 값 = " << (int)ram.read(102) << std::endl;  
+    return 0;
+}
